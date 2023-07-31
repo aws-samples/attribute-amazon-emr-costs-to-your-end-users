f58d909051bee7f90ae6acf4e32e2c3725da987f
Raj Patel <rqpatel@amazon.com>
2023-07-31 19:10:27 +0530

    RDS Tables DDL file added

--- emr-cost-rds-tables-ddl.sql
+++ emr-cost-rds-tables-ddl.sql
@@ -35,20 +35,6 @@ CREATE TABLE public.emr_applications_execution_log (
 );
 
 
-CREATE TABLE public.emr_applications_execution_log_lz (
-	app_id varchar NULL,
-	app_name varchar NULL,
-	queue varchar NULL,
-	job_state varchar NULL,
-	job_status varchar NULL,
-	starttime varchar NULL,
-	endtime varchar NULL,
-	runtime_seconds float8 NULL,
-	vcore_seconds numeric NULL,
-	memory_seconds numeric NULL,
-	running_containers int4 NULL,
-	rm_clusterid varchar NULL
-);
 
 CREATE TABLE public.emr_cluster_usage_cost (
 	costdatecollect date NULL,
@@ -61,5 +47,26 @@ CREATE TABLE public.emr_cluster_usage_cost (
 	service_name varchar(200) NULL,
 	emr_clusterid varchar(30) NULL,
 	emr_clustername varchar(100) NULL,
+	loadtime timestamp NULL
+);
+
+
+CREATE TABLE public.emr_cluster_instances_usage (
+	instancedatecollect date NULL,
+	emr_instance_day_run_seconds numeric NULL,
+	emr_region varchar(30) NULL,
+	emr_clusterid varchar(30) NULL,
+	emr_clustername varchar(100) NULL,
+	emr_cluster_fleet_type varchar(100) NULL,
+	emr_node_type varchar(100) NULL,
+	emr_market varchar(100) NULL,
+	emr_instance_type varchar(100) NULL,
+	emr_ec2_instance_id varchar(100) NULL,
+	emr_ec2_status varchar(100) NULL,
+	emr_ec2_default_vcpus int4 NULL,
+	emr_ec2_memory int4 NULL,
+	emr_ec2_creation_datetime timestamp NULL,
+	emr_ec2_end_datetime timestamp NULL,
+	emr_ec2_ready_datetime timestamp NULL,
 	loadtime timestamp NULL
 );
